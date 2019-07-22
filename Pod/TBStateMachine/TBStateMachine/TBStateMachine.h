49c7bbfc521d703f72a23714a7bc121be57f0121
Carles Roig <carles.roig@betvictor.com>
2019-07-22 12:47:31 +0200

    Add Carthage support

---
+++ Pod/TBStateMachine/TBStateMachine/TBStateMachine.h
@@ -0,0 +1,19 @@
+//
+//  TBStateMachine.h
+//  TBStateMachine
+//
+//  Created by Carles Roig on 22/07/2019.
+//  Copyright © 2019 Carles Roig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBStateMachine.
+FOUNDATION_EXPORT double TBStateMachineVersionNumber;
+
+//! Project version string for TBStateMachine.
+FOUNDATION_EXPORT const unsigned char TBStateMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBStateMachine/PublicHeader.h>
+
+
